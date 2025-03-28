2850a5819c0318b8f96332b316a045017d51e619
Francisco Pires <franciscop11@outlook.pt>
2025-03-28 17:07:05 +0000

    Criação dos Controllers da API e teste inicial no Controller "ApiEscolaController". Criação da pasta DTO na estrutura do projeto.

---
+++ Backend/Backend/Controllers/API/ApiUtilizadoresController.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Data;
+using Backend.DTO;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiUtilizadoresController : ControllerBase {
+        // ApplicationDbContext é a classe que representa a base de dados
+        private readonly ApplicationDbContext _context;
+
+        public ApiUtilizadoresController(ApplicationDbContext context) {
+            _context = context;
+        }
+    }
+}
---
+++ Backend/Backend/DTO/EscolaDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.DTO {
+    public class EscolaDTO {
+        public int IdEscola { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string? Localizacao { get; set; }
+    }
+}

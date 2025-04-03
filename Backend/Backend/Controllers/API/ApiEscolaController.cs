9a86f3649d77f5bb1ec77a015889a840e6d9ea2d
Francisco Pires <franciscop11@outlook.pt>
2025-04-03 17:59:24 +0100

    Criação dos controllers para a API e adição do UtilizadorDTO.

---
+++ Backend/Backend/Controllers/API/ApiEscolaController.cs
@@ -0,0 +1,32 @@
+﻿using Backend.Data;
+using Backend.DTO;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiEscolaController : ControllerBase {
+        // ApplicationDbContext é a classe que representa a base de dados
+        private readonly ApplicationDbContext _context;
+
+        public ApiEscolaController(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetAllEscolas")]
+        public ActionResult<IEnumerable<EscolaDTO>> GetEscolas() {
+
+            // Obter todas as escolas da base de dados
+            var escolas = _context.Escolas.Select(
+                e => new EscolaDTO {
+                    IdEscola = e.IdEscola,
+                    Nome = e.Nome,
+                    Localizacao = e.Localizacao
+                }).ToList();
+            return Ok(escolas);
+        }
+    }
+}
---
+++ Backend/Backend/Controllers/API/ApiTurmasController.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Data;
+using Backend.DTO;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiTurmasController : ControllerBase {
+        // ApplicationDbContext é a classe que representa a base de dados
+        private readonly ApplicationDbContext _context;
+
+        public ApiTurmasController(ApplicationDbContext context) {
+            _context = context;
+        }
+    }
+}

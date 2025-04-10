33f4236a1e365200284092d69e92ad8a53191259
Francisco Pires <franciscop11@outlook.pt>
2025-04-10 22:37:24 +0100

    CRUD Blocos Horario

---
+++ Backend/Backend/DTO/BlocoHorarioDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Backend.DTO
+{
+    public class BlocoHorarioDTO
+    {
+        public int IdBloco { get; set; }
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFim { get; set; }
+        public DayOfWeek DiaSemana { get; set; }
+        public string ProfessorNome { get; set; }
+        public string DisciplinaNome { get; set; }
+        public string SalaNome { get; set; }
+        public string Tipologia { get; set; }
+        public string TurmaNome { get; set; }
+    }
+}
